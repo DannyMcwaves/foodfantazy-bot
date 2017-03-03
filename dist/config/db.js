2e0b8d4d21fa074d7a0e362448d16139a3701b23
DannyMcwaves <dannymcwaves96@rocketmail.com>
2017-03-03 23:08:02 +0000

    comfigured

--- dist/config/db.js
+++ dist/config/db.js
@@ -11,7 +11,7 @@ var _mongoose2 = _interopRequireDefault(_mongoose);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var DBurl = 'mongodb://heroku_6qp6pwvz:8vbp81kmm8gs6013ni2p9pojdp@ds113580.mlab.com:13580/heroku_6qp6pwvz';
-//var DBurl = 'mongodb://localhost:27017/content';
+//var DBurl = 'mongodb://localhost:27/content';
 _mongoose2.default.connect(DBurl);
 
 var db = _mongoose2.default.connection;
