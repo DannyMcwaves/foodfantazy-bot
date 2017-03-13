ad9f9c18f6fb6004e343f5f19d83bbdea8fdb9d9
DannyMcwaves <dannymcwaves96@rocketmail.com>
2017-03-13 19:12:58 +0000

    finally done

--- dist/fb.js
+++ dist/fb.js
@@ -10,7 +10,7 @@ Object.defineProperty(exports, "__esModule", {
 // this is the facebook script.
 
 var fb = require("fbgraph"),
-    accessToken = "EAADgche8ZAXYBAOLGWM40ERAka8EG6mrgTulypa8yR1Fi38ZBWc2yyrEfvGkQd2f2AJvOXRNelq6vvfl9HtvmbMZAfuLZClFoZA410st0O45dW4XXmU0MFZB5iA0KGZCFbzBwSc6W92kREuBU12UbbuBNHjH0Pcyiz9PrhBPPLrStlLpPl4FEadwwZAgkc7Vt7MZD",
+    accessToken = "EAADgche8ZAXYBADlmFQKG49aA82hmmlka6SEVdFOYR0xHZBvYHJSNaDd1KHm7BgISRMMAPqpZCsuxSXejhXQAF3ZCciJZBzPd3NZCY3SAuod3nJL8wxLZBcA0ZClQBNYjxIiUgQsiriE9Db1UkAyKNStaSd3rvD2Iv6OnilrECZAvdQZDZD",
     id = "246780629116278";
 
 fb.setAccessToken(accessToken);
@@ -18,7 +18,7 @@ fb.setAccessToken(accessToken);
 var Facebook = function Facebook(obj) {
     "use strict";
 
-    fb.post(id+"/photos", {
+    fb.post("me/photos", {
         url: obj.url,
         caption: obj.title + " #REVELATOR #horrorfilm #horror #indiefilm #film #movie #trailer #SupportIndieFilm #ComingSoon #horrormovies #horrormovie #staytuned #ghost #creepy #spooky #boo #halloween #streaming #cinema #filmmaking #itunes #netflix #follow #rt"
     }, function (err, res) {
